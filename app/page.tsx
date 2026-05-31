ef0419c29cdf65b74d29f9819eb826f97b02f2a2
Busayo Tella <114032372+Donbishop19@users.noreply.github.com>
2026-05-31 05:34:05 -0700

    Assets and Metadata

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,10 @@
-const page = () => {
+const Home = () => {
   return (
     <div>
-      <h1 className="text-3xl">Welcome to Ultimate Next.js Course</h1>
+      <h1 className="h1-bold font-inter">Welcome to Ultimate Next.js Course</h1>
+      <h1 className="h1-bold font-space-grotesk">welcome to the world of next.js (Inter) </h1>
     </div>
-  )
-}
+  );
+};
 
-export default page
+export default Home;
