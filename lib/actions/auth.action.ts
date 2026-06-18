0a22f9bc43ad286661e1acb4aafbf67de0cf3df4
Busayo Tella <114032372+Donbishop19@users.noreply.github.com>
2026-06-18 10:03:11 -0700

    Implement Password/Email Auth

---
+++ lib/actions/auth.action.ts
@@ -0,0 +1,62 @@
+"use server";
+
+import  mongoose  from "mongoose";
+import action from "../handlers/action";
+import handleError from "../handlers/error";
+import { SignInSchema } from "../validations";
+import User from "@/database/user.model";
+import bcrypt from "bcryptjs";
+import Account from "@/database/account.model";
+import { signIn } from "@/auth";
+
+export async function signUpWithCredentials(params: AuthCredentials): Promise<ActionResponse> {
+  const validationResult = await action({ params, schema: SignInSchema });
+
+  if(validationResult instanceof Error) {
+    return handleError(validationResult) as ErrorResponse;
+  } 
+
+  const {name, username, email, password} = validationResult.params!;
+
+  const session = await mongoose.startSession();
+  session.startTransaction();
+
+  try {
+    const existingUser = await User.findOne({ email }).session(session);
+
+    if(existingUser) {
+      throw new Error("User already exist");
+    }
+
+    const existingUsername = await User.findOne({ username }).session(session);
+
+    if(existingUsername) {
+      throw new Error("Username already exist")
+    }
+
+    const hashedPassword = await bcrypt.hash(password, 12);
+    
+    const [ newUser ] = await User.create([{username, name, email}], { session });
+
+    await Account.create([{ 
+      userId: newUser._id,
+      name,
+      provider: "credentials",
+      providerAccountId: email,
+      password: hashedPassword,
+    }]);
+
+    await session.commitTransaction();
+    
+    await signIn("credentials", { email,password, redirect: false
+     });
+     
+     return { success: true };
+  } catch (error) {
+    session.abortTransaction();
+
+    return handleError(error) as ErrorResponse;
+  } finally {
+    await session.endSession();
+  }
+};
