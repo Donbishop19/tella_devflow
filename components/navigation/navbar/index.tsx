5947635f28e65e2816cee647de489bf026fb4f60
Busayo Tella <114032372+Donbishop19@users.noreply.github.com>
2026-06-19 13:53:05 -0700

    Implement Logout and show User Info  on Home

--- components/navigation/navbar/index.tsx
+++ components/navigation/navbar/index.tsx
@@ -2,8 +2,11 @@ import Link from "next/link"
 import Image from "next/image"
 import Theme from "./Theme"
 import MobileNavigation from "./MobileNavigation"
+import { auth } from "@/auth"
+import UserAvatar from "@/components/UserAvatar";
 
-const Navbar = () => {
+const Navbar = async () => {
+  const session = await auth();
   return (
     <nav className="flex-between background-light900_dark200 fixed z-50 w-full p-6 dark:shadow-none sm:px-12 gap-5">
       <Link href="/" className="flex items-center gap-1">
@@ -23,6 +26,14 @@ const Navbar = () => {
 
       <div className="flex-between gap-5">
         <Theme />
+
+        {session?.user?.id && (
+          <UserAvatar
+            id={session.user.id}
+            name={session.user.name!}
+            imageUrl={session.user?.image}
+          />
+        )}
 
         <MobileNavigation />
       </div>
--- next.config.ts
+++ next.config.ts
@@ -9,7 +9,18 @@ const nextConfig: NextConfig = {
         protocol: "https",
         hostname: "static.vecteezy.com",
         port: "",
+      },
+      {
+        protocol: "https",
+        hostname:  "lh3.googleusercontent.com",
+        port: "",
+      },
+      {
+        protocol: "https",
+        hostname: "avatars.githubusercontent.com",
+        port: "",
       }
+      
     ]
   }
 };
