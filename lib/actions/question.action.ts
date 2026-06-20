ac2291ecff4e529d1aab8d9783356a3fccb1bae1
Busayo Tella <114032372+Donbishop19@users.noreply.github.com>
2026-06-20 06:53:26 -0700

    Implement Create Question Form Logic

--- lib/actions/question.action.ts
+++ lib/actions/question.action.ts
@@ -8,62 +8,73 @@ import Question from "@/database/question.model";
 import Tag from "@/database/tag.model";
 import TagQuestion from "@/database/tag.question";
 
-export async function createQuestion(params: createQuestionParams): Promise<ActionResponse> {
-  const validationResult = await action({ params, schema: AskQuestionSchema, authorize: true });
+export async function createQuestion(
+  params: createQuestionParams
+): Promise<ActionResponse<Question>> {
+  const validationResult = await action({
+    params,
+    schema: AskQuestionSchema,
+    authorize: true,
+  });
 
-  if(validationResult instanceof Error) {
+  if (validationResult instanceof Error) {
     return handleError(validationResult) as ErrorResponse;
-  } 
+  }
 
-  const { title,content, tags } = validationResult.params;
+  const { title, content, tags } = validationResult.params!;
   const userId = validationResult?.session?.user?.id;
-  
+
   const session = await mongoose.startSession();
   session.startTransaction();
 
   try {
-    const [question] = await Question.create([{ title, content, author: userId }], { session });
+    const [question] = await Question.create(
+      [{ title, content, author: userId }],
+      { session }
+    );
 
-    if(!question) {
-      throw new Error('Failed to create question');
+    if (!question) {
+      throw new Error("Failed to create question");
     }
 
     const tagIds: mongoose.Types.ObjectId[] = [];
-    const tagQuestionDocument = [];
+    const tagQuestionDocuments = [];
 
-    function escapeRegex(str: string): string {
+     function escapeRegex(str: string): string {
      return str.replace(/[.*+?^${}()|[\]\\]/g, '\\$&');
     }
 
     for (const tag of tags) {
       const existingTag = await Tag.findOneAndUpdate(
-        {  name: { $regex: new RegExp(`^${escapeRegex(tag)}$`, "i") },}, 
-        { $setOnInsert: { name: tag }, $inc: { question: 1 } }, 
-        {  upsert: true, new: true, session} 
+        { name: { $regex: new RegExp(`^${escapeRegex(tag)}$`, "i") } },
+        { $setOnInsert: { name: tag }, $inc: { questions: 1 } },
+        { upsert: true, new: true, session }
       );
 
       tagIds.push(existingTag._id);
-      tagQuestionDocument.push({
+      tagQuestionDocuments.push({
         tag: existingTag._id,
-        question: question._id
-      })
+        question: question._id,
+      });
     }
 
-    await TagQuestion.insertMany(tagQuestionDocument, { session });
+    await TagQuestion.insertMany(tagQuestionDocuments, { session });
 
     await Question.findByIdAndUpdate(
       question._id,
       { $push: { tags: { $each: tagIds } } },
       { session }
     );
+
     await session.commitTransaction();
 
-    return { success: true,data: JSON.parse(JSON.stringify(question)) }
+    return { success: true, data: JSON.parse(JSON.stringify(question)) };
   } catch (error) {
-    await session.abortTransaction();
+    if (session.inTransaction()) {
+      await session.abortTransaction();
+    }
     return handleError(error) as ErrorResponse;
   } finally {
     session.endSession();
   }
-
 }
