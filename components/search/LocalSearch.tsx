8bb88633d2ba131a642166d08b96a5cc24a06c09
Busayo Tella <114032372+Donbishop19@users.noreply.github.com>
2026-06-23 03:38:06 -0700

    Display Tags

--- components/search/LocalSearch.tsx
+++ components/search/LocalSearch.tsx
@@ -13,16 +13,23 @@ interface Props {
   imgSrc: string;
   placeholder: string;
   otherClasses?: string;
+  iconPosition?: "left" | "right";
 }
 
-const LocalSearch = ({ route, imgSrc, placeholder, otherClasses }: Props) => {
+const LocalSearch = ({
+  route,
+  imgSrc,
+  placeholder,
+  otherClasses,
+  iconPosition = "left",
+}: Props) => {
   const pathname = usePathname();
   const router = useRouter();
   const searchParams = useSearchParams();
   const query = searchParams.get("query") || "";
 
-
-  const [searchQuery, setSearchQuery] = useState(query); 
+  const [searchQuery, setSearchQuery] = useState(query);
+  
   const previousSearchRef = useRef(searchQuery);
   
    // Sync local state when URL query parameter changes
@@ -44,29 +51,34 @@ const LocalSearch = ({ route, imgSrc, placeholder, otherClasses }: Props) => {
         });
 
         router.push(newUrl, { scroll: false });
-      } else {  
-        const newUrl = removeKeysFromQuery({
-          params: searchParams.toString(),
-          keysToRemove: ["query"],
-        });
+      } else {
+        if (pathname === route) {
+          const newUrl = removeKeysFromQuery({
+            params: searchParams.toString(),
+            keysToRemove: ["query"],
+          });
 
           router.push(newUrl, { scroll: false });
+        }
       }
     }, 300);
 
     return () => clearTimeout(delayDebounceFn);
   }, [searchQuery, router, route, searchParams, pathname]);
+
   return (
     <div
       className={`background-light800_darkgradient flex min-h-14 grow items-center gap-4 rounded-[10px] px-4 ${otherClasses}`}
     >
-      <Image
-        src={imgSrc}
-        width={24}
-        height={24}
-        alt="Search"
-        className="cursor-pointer"
-      />
+      {iconPosition === "left" && (
+        <Image
+          src={imgSrc}
+          width={24}
+          height={24}
+          alt="Search"
+          className="cursor-pointer"
+        />
+      )}
 
       <Input
         type="text"
@@ -75,6 +87,16 @@ const LocalSearch = ({ route, imgSrc, placeholder, otherClasses }: Props) => {
         onChange={(e) => setSearchQuery(e.target.value)}
         className="paragraph-regular no-focus placeholder text-dark400_light700 border-none shadow-none outline-none"
       />
+
+      {iconPosition === "right" && (
+        <Image
+          src={imgSrc}
+          width={15}
+          height={15}
+          alt="Search"
+          className="cursor-pointer"
+        />
+      )}
     </div>
   );
 };
