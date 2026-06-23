eea2303b3b5606b0e9df019055e1876d063d95d4
Busayo Tella <114032372+Donbishop19@users.noreply.github.com>
2026-06-23 13:27:56 -0700

    Display Tag Specific Questions

--- app/(root)/tags/page.tsx
+++ app/(root)/tags/page.tsx
@@ -38,7 +38,7 @@ const Tags = async ({ searchParams }: RouteParams) => {
         render={(tags) => (
           <div className="mt-10 flex w-full flex-wrap gap-4">
             {tags.map((tag) => (
-              <TagCard key={tag._id} {...tag} compact />
+              <TagCard key={tag._id} {...tag} />
             ))}
           </div>
         )}
