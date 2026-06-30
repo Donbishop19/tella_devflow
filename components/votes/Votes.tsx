f8f1557d162478497be5237614f745a4fcd40901
Busayo Tella <114032372+Donbishop19@users.noreply.github.com>
2026-06-30 04:18:00 -0700

    Implement Answer Votes

--- components/votes/Votes.tsx
+++ components/votes/Votes.tsx
@@ -1,36 +1,44 @@
 "use client";
 
-import { createVote } from '@/lib/actions/vote.action';
-import { formatNumber } from '@/lib/utils';
-import { useSession } from 'next-auth/react';
-import Image from 'next/image';
-import { use, useState } from 'react';
-import { toast } from 'sonner';
+import Image from "next/image";
+import { useSession } from "next-auth/react";
+import { use, useState } from "react";
+
+import { toast } from "sonner";
+import { createVote } from "@/lib/actions/vote.action";
+import { formatNumber } from "@/lib/utils";
 
 interface Params {
   targetType: "question" | "answer";
   targetId: string;
   upvotes: number;
   downvotes: number;
-  hasVotedPromise: Promise<ActionResponse<HasVotedResponse>>
+  hasVotedPromise: Promise<ActionResponse<HasVotedResponse>>;
 }
 
-const Votes = ({ upvotes, downvotes, hasVotedPromise,   targetId, targetType }: Params) => {
+const Votes = ({
+  upvotes,
+  downvotes,
+  hasVotedPromise,
+  targetId,
+  targetType,
+}: Params) => {
   const session = useSession();
   const userId = session.data?.user?.id;
 
   const { success, data } = use(hasVotedPromise);
 
-  const [isLoading, setisLoading] = useState(false);
+  const [isLoading, setIsLoading] = useState(false);
 
   const { hasUpvoted, hasDownvoted } = data || {};
 
   const handleVote = async (voteType: "upvote" | "downvote") => {
-    if(!userId) return toast("Please login to vote", {
-      description: "Only logged-in user can vote"
-    });
+    if (!userId)
+      return toast("Please login to vote", { 
+        description: "Only logged-in users can vote.",
+      });
 
-    setisLoading(true);
+    setIsLoading(true);
 
     try {
       const result = await createVote({
@@ -40,63 +48,73 @@ const Votes = ({ upvotes, downvotes, hasVotedPromise,   targetId, targetType }: 
       });
 
       if (!result.success) {
-        return toast("Failed to vote", {
+        return toast("Failed to vote", { 
           description: result.error?.message,
         });
       }
 
-      const successMessage = voteType === "upvote" 
-      ? `Upvote ${hasUpvoted ? "removed" : "added"} successfully`
-      : `Downvote ${hasDownvoted ? "removed" : "added"} successfully`;      toast(successMessage, {
-        description: "Your vote has been recorded."
-      })
+      const successMessage =
+        voteType === "upvote"
+          ? `Upvote ${!hasUpvoted ? "added" : "removed"} successfully`
+          : `Downvote ${!hasDownvoted ? "added" : "removed"} successfully`;
 
+      toast(successMessage, { 
+        description: "Your vote has been recorded.",
+      });
     } catch {
       toast("Failed to vote", {
-        description: "An error occured while voting. please try again later"
+        description: "An error occurred while voting. Please try again later.",
       });
     } finally {
-      setisLoading(false);
+      setIsLoading(false);
     }
   };
 
-  return <div className='flex-center gap-2.5'>
-    <div className="flex-center gap-1.5">
-      <Image 
-        src={success && hasUpvoted ? "/icons/upvoted.svg" : "/icons/upvote.svg"}
-        width={18}
-        height={18}
-        alt='upvote'
-        className={`cursor-pointer ${isLoading && "opacity-50"}`}
-        aria-label='upvote'
-        onClick={() => !isLoading && handleVote("upvote")}
-      />
+  return (
+    <div className="flex-center gap-2.5">
+      <div className="flex-center gap-1.5">
+        <Image
+          src={
+            success && hasUpvoted ? "/icons/upvoted.svg" : "/icons/upvote.svg"
+          }
+          width={18}
+          height={18}
+          alt="upvote"
+          className={`cursor-pointer ${isLoading && "opacity-50"}`}
+          aria-label="Upvote"
+          onClick={() => !isLoading && handleVote("upvote")}
+        />
 
-      <div className="flex-center background-light700_dark400 min-w-5 rounded-sm p-1">
-        <p className='subtle-medium text-dark400_light900'>
-          {formatNumber(upvotes)}
-        </p>
+        <div className="flex-center background-light700_dark400 min-w-5 rounded-sm p-1">
+          <p className="subtle-medium text-dark400_light900">
+            {formatNumber(upvotes)}
+          </p>
+        </div>
+      </div>
+
+      <div className="flex-center gap-1.5">
+        <Image
+          src={
+            success && hasDownvoted
+              ? "/icons/downvoted.svg"
+              : "/icons/downvote.svg"
+          }
+          width={18}
+          height={18}
+          alt="downvote"
+          className={`cursor-pointer ${isLoading && "opacity-50"}`}
+          aria-label="Downvote"
+          onClick={() => !isLoading && handleVote("downvote")}
+        />
+
+        <div className="flex-center background-light700_dark400 min-w-5 rounded-sm p-1">
+          <p className="subtle-medium text-dark400_light900">
+            {formatNumber(downvotes)}
+          </p>
+        </div>
       </div>
     </div>
+  );
+};
 
-    <div className="flex-center gap-1.5">
-      <Image 
-        src={success && hasDownvoted ? "/icons/downvoted.svg" : "/icons/downvote.svg"}
-        width={18}
-        height={18}
-        alt='downvote'
-        className={`cursor-pointer ${isLoading && "opacity-50"}`}
-        aria-label='downvote'
-        onClick={() => !isLoading && handleVote("downvote")}
-      />
-
-      <div className="flex-center background-light700_dark400 min-w-5 rounded-sm p-1">
-        <p className='subtle-medium text-dark400_light900'>
-          {formatNumber(downvotes)}
-        </p>
-      </div>
-    </div>
-  </div>
-}
-
-export default Votes
+export default Votes;
--- lib/actions/vote.action.ts
+++ lib/actions/vote.action.ts
@@ -1,61 +1,73 @@
 "use server";
 
 import mongoose, { ClientSession } from "mongoose";
+import { revalidatePath } from "next/cache";
+
+import ROUTES from "@/constants/routes";
+import { Answer, Question, Vote } from "@/database";
+
 import action from "../handlers/action";
 import handleError from "../handlers/error";
-import { CreateVoteSchema, HasVotedSchema, UpdateVoteCountSchema } from "../validations";
-import { Answer, Question, Vote } from "@/database";
-import { revalidatePath } from "next/cache";
-import ROUTES from "@/constants/routes";
+import {
+  CreateVoteSchema,
+  HasVotedSchema,
+  UpdateVoteCountSchema,
+} from "../validations";
 
- export async function updateVoteCount(params: UpdateVoteCountParams, session?: ClientSession): Promise<ActionResponse> {
+export async function updateVoteCount(
+  params: UpdateVoteCountParams,
+  session?: ClientSession
+): Promise<ActionResponse> {
   const validationResult = await action({
     params,
     schema: UpdateVoteCountSchema,
   });
 
-  if(validationResult instanceof Error) {
+  if (validationResult instanceof Error) {
     return handleError(validationResult) as ErrorResponse;
   }
 
   const { targetId, targetType, voteType, change } = validationResult.params!;
 
   const Model = targetType === "question" ? Question : Answer;
-
   const voteField = voteType === "upvote" ? "upvotes" : "downvotes";
 
   try {
-    const result = await Model.findByIdAndUpdate(targetId, 
-      { $inc: { [voteField]: change }},
-      {new : true, session}
+    const result = await Model.findByIdAndUpdate(
+      targetId,
+      { $inc: { [voteField]: change } },
+      { new: true, session }
     );
 
-    if (!result) return handleError(new Error("Failed to update vote count")) as ErrorResponse;
+    if (!result)
+      return handleError(
+        new Error("Failed to update vote count")
+      ) as ErrorResponse;
 
-    return { success: true }
+    return { success: true };
   } catch (error) {
     return handleError(error) as ErrorResponse;
   }
-}  
+}
 
-export async function createVote(params: CreateVoteParams): Promise<ActionResponse> {
+export async function createVote(
+  params: CreateVoteParams
+): Promise<ActionResponse> {
   const validationResult = await action({
     params,
     schema: CreateVoteSchema,
     authorize: true,
   });
 
-  if(validationResult instanceof Error) {
+  if (validationResult instanceof Error) {
     return handleError(validationResult) as ErrorResponse;
   }
 
   const { targetId, targetType, voteType } = validationResult.params!;
-
   const userId = validationResult.session?.user?.id;
 
-  if (!userId) {
-    return handleError(new Error("unauthorized")) as ErrorResponse;
-  }
+  if (!userId) return handleError(new Error("Unauthorized")) as ErrorResponse;
+
   const session = await mongoose.startSession();
   session.startTransaction();
 
@@ -63,33 +75,48 @@ export async function createVote(params: CreateVoteParams): Promise<ActionRespon
     const existingVote = await Vote.findOne({
       author: userId,
       actionId: targetId,
-      actionType: targetType
+      actionType: targetType,
     }).session(session);
 
     if (existingVote) {
       if (existingVote.voteType === voteType) {
-        // if the user has already voted with the same voteType, remove the vote
+        // If the user has already voted with the same voteType, remove the vote
         await Vote.deleteOne({ _id: existingVote._id }).session(session);
-        await updateVoteCount({ targetId, targetType, voteType, change: -1 }, session);
-        // if the user has already voted with a different voteType, update the vote
+        await updateVoteCount(
+          { targetId, targetType, voteType, change: -1 },
+          session
+        );
+      } else {
+        // If the user has already voted with a different voteType, update the vote
         await Vote.findByIdAndUpdate(
           existingVote._id,
           { voteType },
           { new: true, session }
         );
-        await updateVoteCount({
-          targetId,
-          targetType,
-          voteType: existingVote.voteType,
-          change: -1,
-        }, session);
-        await updateVoteCount({ targetId, targetType, voteType, change: 1 }, session)
+        await updateVoteCount(
+          { targetId, targetType, voteType: existingVote.voteType, change: -1 },
+          session
+        );
+        await updateVoteCount(
+          { targetId, targetType, voteType, change: 1 },
+          session
+        );
       }
     } else {
-      // if the user has not voted yet, create a new vote
-      await Vote.create([{ author: userId, actionId: targetId, actionType: targetType, voteType }], { session });
-
-      
+      // If the user has not voted yet, create a new vote
+      await Vote.create(
+        [
+          {
+            author: userId,
+            actionId: targetId,
+            actionType: targetType,
+            voteType,
+          },
+        ],
+        {
+          session,
+        }
+      );
       const countResult = await updateVoteCount({ targetId, targetType, voteType, change: -1 }, session);
       if (!countResult.success) {
         throw new Error("Failed to update vote count");
@@ -101,23 +128,24 @@ export async function createVote(params: CreateVoteParams): Promise<ActionRespon
 
     revalidatePath(ROUTES.QUESTION(targetId));
 
-    return { success: true }
+    return { success: true };
   } catch (error) {
     await session.abortTransaction();
     session.endSession();
     return handleError(error) as ErrorResponse;
   }
-
 }
 
-export async function hasVoted(params: HasVotedParams): Promise<ActionResponse<HasVotedResponse>> {
-  const validationResult = await action( {
+export async function hasVoted(
+  params: HasVotedParams
+): Promise<ActionResponse<HasVotedResponse>> {
+  const validationResult = await action({
     params,
     schema: HasVotedSchema,
     authorize: true,
-  })
+  });
 
-  if(validationResult instanceof Error) {
+  if (validationResult instanceof Error) {
     return handleError(validationResult) as ErrorResponse;
   }
 
@@ -128,11 +156,14 @@ export async function hasVoted(params: HasVotedParams): Promise<ActionResponse<H
     const vote = await Vote.findOne({
       author: userId,
       actionId: targetId,
-      actionType: targetType
+      actionType: targetType,
     });
 
-    if (!vote) 
-      return { success: false, data: { hasUpvoted: false, hasDownvoted: false }
+    if (!vote) {
+      return {
+        success: false,
+        data: { hasUpvoted: false, hasDownvoted: false },
+      };
     }
 
     return {
@@ -140,8 +171,8 @@ export async function hasVoted(params: HasVotedParams): Promise<ActionResponse<H
       data: {
         hasUpvoted: vote.voteType === "upvote",
         hasDownvoted: vote.voteType === "downvote",
-      }
-    }
+      },
+    };
   } catch (error) {
     return handleError(error) as ErrorResponse;
   }
